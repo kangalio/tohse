ab52a30288d42192084a8ef6430cc5b9c666fcea
kangalio <me@kangalio.net>
2025-07-27 01:02:15 +0200

    Allow sleep during replay to skip yield to catch up
    Which allows super fast replay viewing xD In essence, this results in multiple moves being made inbetween renders, instead of each move being followed by a forced browser render

--- src/util/abortableSleep.ts
+++ src/util/abortableSleep.ts
@@ -1,4 +1,5 @@
-export const abortableSleep = (secs: number, signal?: AbortSignal): Promise<void> => {
+export const abortableSleep = (secs: number, signal?: AbortSignal): Promise<void> | void => {
+    if (secs <= 0) return;
     return new Promise(resolve => {
         const timeoutId = setTimeout(resolve, secs * 1000);
         
