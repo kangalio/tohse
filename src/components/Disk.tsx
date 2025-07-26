06a5793882cd41bebe4486bf28768703b53ff189
kangalio <me@kangalio.net>
2025-07-26 20:58:26 +0200

    Fix disk number outline artifacts

--- src/components/Disk.tsx
+++ src/components/Disk.tsx
@@ -20,8 +20,12 @@ export const Disk = styled.div.attrs<Props>(({ $color, $width, $height }) => ({
     border-radius: 10px;
     margin: ${p => (p.$holding ? '20px' : 'auto')} auto auto auto;
     font-size: ${p => p.$height - 10}px;
-    -webkit-text-stroke-width: 1px;
-    -webkit-text-stroke-color: black;
+    // -webkit-text-stroke causes artifacts https://stackoverflow.com/q/76915835/9946772
+    text-shadow: 
+        -1px -1px 0 #000,  
+        1px -1px 0 #000,
+        -1px 1px 0 #000,
+        1px 1px 0 #000;
     display: flex;
     justify-content: center;
     align-items: center;
